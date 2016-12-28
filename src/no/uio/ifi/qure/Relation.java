c9051b709964d50c6e84ee96557bf73bb7e28141
leifhka <leifharaldkarlsen@gmail.com>
2016-12-28 12:46:24 +0100

    Updated mail.

--- src/no/uio/ifi/qure/Relation.java
+++ src/no/uio/ifi/qure/Relation.java
@@ -15,13 +15,13 @@ package no.uio.ifi.qure;
  *   partOf(1,2).and(overlaps(2,3)).and(not(partOf(1,3)))
  */
 
-public interface Relation {
+public abstract class Relation {
 
-    public String toSQL();
+    public abstract String toSQL();
 
-    public boolean eval(Space[] args);
+    public abstract boolean eval(Space[] args);
 
-    public default boolean eval(Space s1, Space s2) {
+    public boolean eval(Space s1, Space s2) {
         return eval(new Space[]{s1,s2});
     }
 
@@ -34,87 +34,87 @@ public interface Relation {
     public Relation and(Relation o) { return new And(this, o); }
 
     public static Relation not(Relation o) { return new Not(o); }
+}
 
-    class And implements Relation {
-    
-        private Relation conj1, conj2;
-    
-        public And(Relation conj1, Relation conj2) {
-            this.conj1 = conj1;
-            this.conj2 = conj2;
-        }
-    
-        public String toSQL() {
-            return ""; //TODO
-        }
-    
-        public boolean eval(Space[] args) {
-            return conj1.eval(args) && conj2.eval(args);
-        }
+class And extends Relation {
+
+    private Relation conj1, conj2;
+
+    public And(Relation conj1, Relation conj2) {
+        this.conj1 = conj1;
+        this.conj2 = conj2;
     }
-    
-    class Not implements Relation {
-    
-        private Relation rel;
-    
-        public Not(Relation rel) {
-            this.rel = rel;
-        }
-    
-        public String toSQL() {
-            return ""; //TODO
-        }
-    
-        public boolean eval(Space[] args) {
-            return !rel.eval(args);
-        }
+
+    public String toSQL() {
+        return ""; //TODO
     }
-    
-    class Overlaps implements Relation {
 
-        private final int[] args;
-
-        public Overlaps(int a1, int a2) {
-            this.args = new int[]{a1, a2};
-        }
-    
-        public Overlaps(int[] args) {
-            if (args.length < 2) {
-                System.err.println("ERROR: Overlaps needs at least 2 arguments!");
-                System.exit(1);
-            }
-            this.args = args;
-        }
-    
-        public String toSQL() {
-            return ""; //TODO
-        }
-    
-        public boolean eval(Space[] spaceArgs) {
-            
-            Space s = spaceArgs[args[0]];
-    
-            for (int i = 0; i < args.length; i++)
-                s = s.intersection(spaceArgs[args[i]]);
-    
-            return !s.isEmpty();
-        }
+    public boolean eval(Space[] args) {
+        return conj1.eval(args) && conj2.eval(args);
     }
-   
-    class PartOf implements Relation {
+}
 
-        private final int[] args;
-    
-        public PartOf(int a1, int a2) {
-            this.args = new int[]{a1, a2};
+class Not extends Relation {
+
+    private Relation rel;
+
+    public Not(Relation rel) {
+        this.rel = rel;
+    }
+
+    public String toSQL() {
+        return ""; //TODO
+    }
+
+    public boolean eval(Space[] args) {
+        return !rel.eval(args);
+    }
+}
+
+class Overlaps extends Relation {
+
+    private final int[] args;
+
+    public Overlaps(int a1, int a2) {
+        this.args = new int[]{a1, a2};
+    }
+
+    public Overlaps(int[] args) {
+        if (args.length < 2) {
+            System.err.println("ERROR: Overlaps needs at least 2 arguments!");
+            System.exit(1);
         }
-    
-        public String toSQL() {
-            return ""; //TODO
-        }
-    
-        public boolean eval(Space[] spaceArgs) {
-            return spaceArgs[args[0]].covers(spaceArgs[args[1]]);
-        }
+        this.args = args;
+    }
+
+    public String toSQL() {
+        return ""; //TODO
+    }
+
+    public boolean eval(Space[] spaceArgs) {
+        
+        Space s = spaceArgs[args[0]];
+
+        for (int i = 0; i < args.length; i++)
+            s = s.intersection(spaceArgs[args[i]]);
+
+        return !s.isEmpty();
+    }
+}
+
+class PartOf extends Relation {
+
+    private final int[] args;
+
+    public PartOf(int a1, int a2) {
+        this.args = new int[]{a1, a2};
+    }
+
+    public String toSQL() {
+        return ""; //TODO
+    }
+
+    public boolean eval(Space[] spaceArgs) {
+        return spaceArgs[args[0]].coveredBy(spaceArgs[args[1]]);
     }
 }
