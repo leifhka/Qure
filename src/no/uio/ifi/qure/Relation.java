86133cea1719f0d7329cf7409e105ccb98fae7d7
Leif Harald Karlsen <leifharaldkarlsen@gmail.com>
2016-12-28 11:55:58 +0100

    Added shortcut-methods for Relation-construction.

--- src/no/uio/ifi/qure/Relation.java
+++ src/no/uio/ifi/qure/Relation.java
@@ -1,12 +1,18 @@
 package no.uio.ifi.qure;
 
 /* Example relations:
- *   new And(Not(PartOf(1,2)),
- *           Overlaps(1,2))
+ *   new And(new Not(new PartOf(1,2)),
+ *           new Overlaps(1,2))
+ * or
+ *   not(partOf(1,2)).and(overlaps(1,2))
  *
- *   new And(PartOf(1,2),
- *           And(Overlaps(2,3),
- *               Not(PartOf(1,3))))
+ * --------------------------------------
+ *
+ *   new And(new PartOf(1,2),
+ *           new And(new Overlaps(2,3),
+ *               new Not(new PartOf(1,3))))
+ * or
+ *   partOf(1,2).and(overlaps(2,3)).and(not(partOf(1,3)))
  */
 
 public interface Relation {
@@ -18,6 +24,16 @@ public interface Relation {
     public default boolean eval(Space s1, Space s2) {
         return eval(new Space[]{s1,s2});
     }
+
+    public static Relation overlaps(int a1, int a2) { return new Overlaps(a1, a2); }
+
+    public static Relation overlaps(int[] as) { return new Overlaps(as); }
+
+    public static Relation partOf(int a1, int a2) { return new PartOf(a1, a2); }
+
+    public Relation and(Relation o) { return new And(this, o); }
+
+    public static Relation not(Relation o) { return new Not(o); }
 
     class And implements Relation {
     
@@ -76,8 +92,6 @@ public interface Relation {
     
         public boolean eval(Space[] spaceArgs) {
             
-            if (spaceArgs.length == 0) return false;
-            
             Space s = spaceArgs[args[0]];
     
             for (int i = 0; i < args.length; i++)
@@ -100,9 +114,6 @@ public interface Relation {
         }
     
         public boolean eval(Space[] spaceArgs) {
-            
-            if (spaceArgs.length < 2) return false;
-            
             return spaceArgs[args[0]].covers(spaceArgs[args[1]]);
         }
     }
