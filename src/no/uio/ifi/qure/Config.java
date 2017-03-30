b960bdc45d79ed5456adfc948de6e78a1ff341cc
Leif Harald Karlsen <leifharaldkarlsen@gmail.com>
2017-03-30 15:33:31 +0200

    Fixed spacing (spaces vs tabs)

--- src/no/uio/ifi/qure/Config.java
+++ src/no/uio/ifi/qure/Config.java
@@ -4,81 +4,81 @@ import com.vividsolutions.jts.geom.PrecisionModel;
 import java.util.function.Predicate;
 
 public class Config {
-    public String rawGeoTableName;
-    public String geoTableName;
-    public String rawBTTableName;
-    public String btTableName;
-    public String geoQuerySelectFromStr;
-    public String geoQueryStr;
-    public String splitTable;
-    public String splitQuery;
-    public String schemaName;
+	public String rawGeoTableName;
+	public String geoTableName;
+	public String rawBTTableName;
+	public String btTableName;
+	public String geoQuerySelectFromStr;
+	public String geoQueryStr;
+	public String splitTable;
+	public String splitQuery;
+	public String schemaName;
 
-    public int maxIterDepth;
-    public int overlapsArity;
-    public int blockMemberCount;
-    public int numThreads = 8;
-    public int dim = 2;
+	public int maxIterDepth;
+	public int overlapsArity;
+	public int blockMemberCount;
+	public int numThreads = 8;
+	public int dim = 2;
 
-    //public int maxDiff = 25;
-    public double minRatio = 0.9;
-    public int maxSplits = 7;
+	//public int maxDiff = 25;
+	public double minRatio = 0.9;
+	public int maxSplits = 7;
  
-    public Config(String table, String suff, int representationDepth, int overlapsArity, 
-                  int blockMemberCount, int maxSplits) {
+	public Config(String table, String suff, int representationDepth, int overlapsArity, 
+				  int blockMemberCount, int maxSplits) {
 
-        this.overlapsArity = overlapsArity;
-        this.maxIterDepth = representationDepth;
-        this.blockMemberCount = blockMemberCount;
-        this.maxSplits = maxSplits;
+		this.overlapsArity = overlapsArity;
+		this.maxIterDepth = representationDepth;
+		this.blockMemberCount = blockMemberCount;
+		this.maxSplits = maxSplits;
 
-        rawGeoTableName = table;
-        geoTableName = "geo." + rawGeoTableName;
-        //geoTableName = rawGeoTableName;
+		rawGeoTableName = table;
+		geoTableName = "geo." + rawGeoTableName;
+		//geoTableName = rawGeoTableName;
 
-        rawBTTableName = rawGeoTableName + "_d" + representationDepth + "_k" + overlapsArity + "_bc" + blockMemberCount + "_ms" + maxSplits + "_" + suff;
-        schemaName = "qure";
-        btTableName = schemaName + "." + rawBTTableName;
+		rawBTTableName = rawGeoTableName + "_d" + representationDepth + "_k" + overlapsArity + "_bc" + blockMemberCount + "_ms" + maxSplits + "_" + suff;
+		schemaName = "qure";
+		btTableName = schemaName + "." + rawBTTableName;
 
-        splitTable = "split." + rawBTTableName;
-        splitQuery = "SELECT * FROM " + splitTable + ";";
+		splitTable = "split." + rawBTTableName;
+		splitQuery = "SELECT * FROM " + splitTable + ";";
 
-        geoQuerySelectFromStr = "SELECT " + uriColumn + ", " + geoColumn + " FROM " + geoTableName;
-        geoQueryStr = geoQuerySelectFromStr + ";";
+		geoQuerySelectFromStr = "SELECT " + uriColumn + ", " + geoColumn + " FROM " + geoTableName;
+		geoQueryStr = geoQuerySelectFromStr + ";";
 
-    }
+	}
 
-    public Predicate<TreeNode> atMaxDepth = new Predicate<TreeNode>() {
-        public boolean test(TreeNode node) {
-            int d = node.getBlock().depth();
-            return d >= maxIterDepth ||
-                   (node.getOverlappingURIs().size() <= blockMemberCount);
-        }
-    };
+	public Predicate<TreeNode> atMaxDepth = new Predicate<TreeNode>() {
+		public boolean test(TreeNode node) {
+			int d = node.getBlock().depth();
+			return d >= maxIterDepth ||
+			       (node.getOverlappingURIs().size() <= blockMemberCount);
+		}
+	};
 
-    public int blockSize = 31;
-    public String dbName = "test";
-    public String dbPWD = "test";
-    public String dbUsername = "leifhka";
-    public String uriColumn = "gid";
-    //public String uriColumn = "trip_id";
-    public String blockColumn = "block";
-    public String geoColumn = "geom";
+	public int blockSize = 31;
+	public String dbName = "test";
+	public String dbPWD = "test";
+	public String dbUsername = "leifhka";
+	public String uriColumn = "gid";
+	//public String uriColumn = "trip_id";
+	public String blockColumn = "block";
+	public String geoColumn = "geom";
 	public int limit = 1000000;
-    //public String geoColumn = "starttime, stoptime";
-    public String universeTable = "qure.universes";
-    public boolean convertUriToInt = true;
+	//public String geoColumn = "starttime, stoptime";
+	public String universeTable = "qure.universes";
+	public boolean convertUriToInt = true;
 
-    public String jdbcDriver = "org.postgresql.Driver";
-    public String connectionStr = "jdbc:postgresql://localhost/" + dbName + "?user=" +
-                                  dbUsername + "&password=" + dbPWD;
+	public String jdbcDriver = "org.postgresql.Driver";
+	public String connectionStr = "jdbc:postgresql://localhost/" + dbName + "?user=" +
+	                              dbUsername + "&password=" + dbPWD;
 
 
-    public boolean verbose = true;
-    public int percentStep = 1;
-    public boolean writeBintreesToDB = true;
+	public boolean verbose = true;
+	public int percentStep = 1;
+	public boolean writeBintreesToDB = true;
 
-    public PrecisionModel geometryFactoryPrecision = new PrecisionModel();
-    public PrecisionModel geometryPrecision = new PrecisionModel(Math.pow(10, 10));
+	public PrecisionModel geometryFactoryPrecision = new PrecisionModel();
+	public PrecisionModel geometryPrecision = new PrecisionModel(Math.pow(10, 10));
 
 }
