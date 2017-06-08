53af3520360fb560ff0c64c3cd43c64eb0b660a8
Leif Harald Karlsen <leifharaldkarlsen@gmail.com>
2017-06-08 15:24:18 +0200

    Removed unused methods.

--- src/no/uio/ifi/qure/relation/AtomicRelation.java
+++ src/no/uio/ifi/qure/relation/AtomicRelation.java
@@ -45,28 +45,5 @@ public abstract class AtomicRelation extends Relation {
 		}
 		return sids;
 	}
-
-	public Set<SID> toSIDSet(Integer[] tuple) {
-		return new HashSet<SID>(Arrays.asList(toSIDs(tuple)));
-	}
-
-	/**
-	 * Returns the set of all lists constructed from the elements of argument tuple,
-	 * assumed to have a max length of 2. Used by PartOf and Before to obtain
-	 * ordered tuples from unordered tuples from Overlaps.
-	 */
-	public Set<List<Integer>> tupleToLists(List<Integer> tuple) {
-
-		Set<List<Integer>> lists = new HashSet<List<Integer>>();
-		lists.add(new ArrayList<Integer>(tuple));
-		
-		if (tuple.size() > 1) {
-			List<Integer> l = new ArrayList<Integer>();
-			l.add(tuple.get(1));
-			l.add(tuple.get(0));
-			lists.add(l);
-		}
-		return lists;
-	}
 }
 
