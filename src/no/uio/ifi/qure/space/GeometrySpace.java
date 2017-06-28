908ba919f761fa9cd03003fdafaf41c140f94ea4
Leif Harald Karlsen <leifharaldkarlsen@gmail.com>
2017-06-28 16:57:07 +0200

    Fixed minor error in epsilon in getPart.

--- src/no/uio/ifi/qure/space/GeometrySpace.java
+++ src/no/uio/ifi/qure/space/GeometrySpace.java
@@ -159,7 +159,7 @@ public class GeometrySpace implements Space {
 
 			// epsilon represents the smallest representable distance with our resolution
 			// Thus, to get the interior of a geometry, we only have to remove eveything in distance epsilon from the boundary
-			double epsilon = Math.pow(10,-precModel.getMaximumSignificantDigits())/2.0; 
+			double epsilon = Math.pow(10,-(precModel.getMaximumSignificantDigits()-1)); 
 			Geometry iGeo;
 
 			if (geo.getGeometryType().equals("MultiPolygon") || geo.getGeometryType().equals("Polygon")) {
